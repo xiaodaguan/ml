953d76561b049765e8e581da31a0b03cd086caaa
guanxiaoda <guanxiaoda@meituan.com>
2018-01-18 10:11:31 +0800

    a

--- knn/kNN.py
+++ knn/kNN.py
@@ -1,3 +1,4 @@
+# encoding=utf-8
 from numpy import *
 import operator
 
@@ -18,14 +19,44 @@ k: 选择近邻数目
 
 def classify0(inX, dataSet, labels, k):
     dataSetSize = dataSet.shape[0]
-    diffMat = tile(inX, (dataSetSize, 1)) - dataSet # 扩展成 4x1 矩阵，和dataset减法
-    sqDiffMat = diffMat ** 2 # 平方
-    sqDistances = sqDiffMat.sum(axis=1) # 求和
-    distances = sqDistances ** 0.5 # 开方，得到距离
-    sortedDistIndices = distances.argsort() # 距离排序
+    diffMat = tile(inX, (dataSetSize, 1)) - dataSet  # 扩展成 4x1 矩阵，和dataset减法
+    sqDiffMat = diffMat ** 2  # 平方
+    sqDistances = sqDiffMat.sum(axis=1)  # 求和
+    distances = sqDistances ** 0.5  # 开方，得到距离
+    sortedDistIndices = distances.argsort()  # 距离排序
     classCount = {}
     for i in range(k):
         voteIlabel = labels[sortedDistIndices[i]]
         classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1  # 计数
-    sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True) # 按count 从大到小排序
+    sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)  # 按count 从大到小排序
     return sortedClassCount[0][0]
+
+
+def file2matrix(filename):
+    fr = open(filename)
+    arrayOLines = fr.readlines()
+    numberOfLines = len(arrayOLines)
+    mat = zeros((numberOfLines, 3))
+    labels = []
+    labelSet = []
+    index = 0
+    for line in arrayOLines:
+        line = line.strip()
+        listFromLine = line.split('\t')
+        mat[index, :] = listFromLine[0:3]
+        if listFromLine[-1] not in labelSet:
+            labelSet.append(listFromLine[-1])
+        labels.append(labelSet.index(listFromLine[-1]))
+        index = index + 1
+    return mat, labels
+
+
+def autoNorm(dataSet):
+    minVals = dataSet.min(0)
+    maxVals = dataSet.max(0)
+    ranges = maxVals - minVals
+    normDataSet = zeros(shape(dataSet))
+    m = dataSet.shape[0]
+    normDataSet = dataSet - tile(minVals, (m, 1))
+    normDataSet = normDataSet / tile(range, (m, 1))
+    return normDataSet, ranges, minVals
--- knn/main.py
+++ knn/main.py
@@ -1,7 +1,24 @@
 import kNN
+import matplotlib
+import numpy
+from numpy import *
+import matplotlib.pyplot as plt
 
-group, labels = kNN.createDataSet()
-print(group)
-print(labels)
+# group, labels = kNN.createDataSet()
+# print(group)
+# print(labels)
 
-print(kNN.classify0([0.8, 0.9], group, labels, 3))
+# print(kNN.classify0([0.8, 0.9], group, labels, 3))
+
+
+group, labels = kNN.file2matrix('datingTestSet.txt')
+# print(group)
+# print(labels)
+#
+# fig = plt.figure()
+# ax = fig.add_subplot(111)
+# ax.scatter(group[:, 1], group[:, 2], 15.0 * array(labels), 15.0 * array(labels))
+# plt.show()
+
+
+normMat, ranges, minVals = kNN.autoNorm(group)
